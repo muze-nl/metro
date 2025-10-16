a4cc4de8fa218dbf6034b37ebc1270f4d51e0a7d
Auke van Slooten <auke@muze.nl>
2025-10-16 15:32:37 +0200

    fixed not overriding content-type, if set to anything other that text/plain (since that is done automatically by the browser)

--- src/mw/json.mjs
+++ src/mw/json.mjs
@@ -1,53 +1,69 @@
-import * as metro from '../metro.mjs'
+export default function jsonmw(options)
+{
+    options = Object.assign({
+        contentType: 'application/json',
+        reviver: null,
+        replacer: null,
+        space: ''
+    }, options)
 
-export default function jsonmw(options) {
-	options = Object.assign({
-		mimetype: 'application/json',
-		reviver: null,
-		replacer: null,
-		space: ''
-	}, options)
-
-	return async function json(req, next) {
-		if (!isJSON(req.headers.get('Accept'))) {
-			req = req.with({
-				headers: {
-	                'Accept': options.mimetype
-	            }
-	        })
-		}
-		if (['POST','PUT','PATCH','QUERY'].includes(req.method)) {
-			if (req.data && typeof req.data=='object' && !(req.data instanceof ReadableStream)) {
-				if (!isJSON(req.headers.get('content-type'))) {
-					req = req.with({
-						headers: {
-							'Content-Type':options.mimetype,
-						}
-					})
-				}
-				req = req.with({
-					body: JSON.stringify(req.data, options.replacer, options.space)
-				})
-			}
-		}
-		let res = await next(req)
-		if (isJSON(res.headers.get('content-type'))) {
-			let tempRes = res.clone()
-			let body = await tempRes.text()
-			try {
-				let json = JSON.parse(body, options.reviver)
-				return res.with({
-					body: json
-				})
-			} catch(e) {
-				// ignore parse errors
-			}
-		} 
-		return res
-	}
+    return async function json(req, next) {
+        if (!req.headers.get('Accept')) {
+            req = req.with({
+                headers: {
+                    'Accept': options.accept ?? options.contentType
+                }
+            })
+        }
+        if (req.method!=='GET' && req.method!=='HEAD') {
+            //https://developer.mozilla.org/en-US/docs/Web/API/Request/body
+            if (req.data && typeof req.data=='object' && !(req.data instanceof ReadableStream)) {
+            		const contentType = req.headers.get('Content-Type')
+                if (!contentType || isPlainText(contentType)) {
+                    req = req.with({
+                        headers: {
+                            'Content-Type':options.contentType,
+                        }
+                    })
+                }
+                if (isJSON(req.headers.get('Content-Type'))) {
+                    req = req.with({
+                        body: JSON.stringify(req.data, options.replacer, options.space)
+                    })
+                }
+            }
+        }
+        let res = await next(req)
+        if (isJSON(res.headers.get('Content-Type'))) {
+            let tempRes = res.clone()
+            let body = await tempRes.text()
+            try {
+                let json = JSON.parse(body, options.reviver)
+                return res.with({
+                    body: json
+                })
+            } catch(e) {
+                // ignore parse errors
+            }
+        }
+        return res
+    }
 }
 
+/*
+  this matches:
+  - application/json
+  - application/ld+json
+  - application/json; charset=utf-8
+  - application/ld+json; charset=utf-8
+*/
 const jsonRE = /^application\/([a-zA-Z0-9\-_]+\+)?json\b/
-function isJSON(contentType) {
-	return jsonRE.exec(contentType)
+function isJSON(contentType)
+{
+    return jsonRE.exec(contentType)
+}
+
+function isPlainText(contentType)
+{
+		return /^text\/plain\b/.exec(contentType)
 }
